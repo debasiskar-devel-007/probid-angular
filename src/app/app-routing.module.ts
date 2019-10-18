416f43b5a074c8c807620732515b2e1f57685f3f
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-10-18 18:01:20 +0530

    koushik

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -79,6 +79,7 @@ const routes: Routes = [
   { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'contactus', component: ContactusComponent },
   {
     path: "testimonialhome",
