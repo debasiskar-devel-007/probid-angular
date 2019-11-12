557f771dca2d1e0bf60c92f8e85aaedf7ec579bc
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-11-12 18:56:06 +0530

    kooushik 20191112 last

--- src/app/component/backend/add-birddog/add-birddog.component.ts
+++ src/app/component/backend/add-birddog/add-birddog.component.ts
@@ -16,26 +16,26 @@ public cityList: any;
 @ViewChild(FormGroupDirective) formDirective: FormGroupDirective;
 constructor(public activatedRouter:ActivatedRoute, public apiservice: ApiService, public fb: FormBuilder,public dialog: MatDialog) { 
     /**genarate Add-birddog form */
-    this.addcustomerForm = this.fb.group({
-      id:null,
-      email: [null, Validators.compose([Validators.required, Validators.pattern(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)])],
-      firstname: [null, Validators.required],
-      lastname: [null, Validators.required],
-      phone: [null, Validators.compose([Validators.required, Validators.pattern(/^0|[1-9]\d*$/)])],
-      zip: [null, Validators.required],
-      city: [null, Validators.required],
-      state: [null, Validators.required],
-      address: [null, Validators.required],
-      password: [null, Validators.required],
-      conpass: [null, Validators.required],
-      type: ["birddog"],
-      status:1
-    }, {
-      validator: this.machpassword('password', 'conpass')
-    });
+    // this.addcustomerForm = this.fb.group({
+    //   id:null,
+    //   email: [null, Validators.compose([Validators.required, Validators.pattern(/^\s*[\w\-\+_]+(\.[\w\-\+_]+)*\@[\w\-\+_]+\.[\w\-\+_]+(\.[\w\-\+_]+)*\s*$/)])],
+    //   firstname: [null, Validators.required],
+    //   lastname: [null, Validators.required],
+    //   phone: [null, Validators.compose([Validators.required, Validators.pattern(/^0|[1-9]\d*$/)])],
+    //   zip: [null, Validators.required],
+    //   city: [null, Validators.required],
+    //   state: [null, Validators.required],
+    //   address: [null, Validators.required],
+    //   password: [null, Validators.required],
+    //   conpass: [null, Validators.required],
+    //   type: ["birddog"],
+    //   status:1
+    // }, {
+    //   validator: this.machpassword('password', 'conpass')
+    // });
 
-    this.getStateList();
-    this.getCityList();
+    // this.getStateList();
+    // this.getCityList();
 }
 
   ngOnInit() {
@@ -46,30 +46,30 @@ constructor(public activatedRouter:ActivatedRoute, public apiservice: ApiService
   }
   
  /**Miss Match password check function */
- machpassword(passwordkye: string, confirmpasswordkye: string) {
-  return (group: FormGroup) => {
-    let passwordInput = group.controls[passwordkye],
-      confirmpasswordInput = group.controls[confirmpasswordkye];
-    if (passwordInput.value !== confirmpasswordInput.value) {
-      return confirmpasswordInput.setErrors({ notEquivalent: true });
-    }
-    else {
-      return confirmpasswordInput.setErrors(null);
-    }
-  };
-}
-getStateList() {
-  this.apiservice.getJsonObject('assets/data/states.json').subscribe(response => {
-    let result: any = {};
-    result = response;
-    this.stateList = result;
-  })
-}
-getCityList() {
-  this.apiservice.getJsonObject('assets/data/usa-cities.json').subscribe((res) => {
-    let result: any = {};
-    result = res;
-    this.cityList = result;
-  })
-}
+//  machpassword(passwordkye: string, confirmpasswordkye: string) {
+//   return (group: FormGroup) => {
+//     let passwordInput = group.controls[passwordkye],
+//       confirmpasswordInput = group.controls[confirmpasswordkye];
+//     if (passwordInput.value !== confirmpasswordInput.value) {
+//       return confirmpasswordInput.setErrors({ notEquivalent: true });
+//     }
+//     else {
+//       return confirmpasswordInput.setErrors(null);
+//     }
+//   };
+// }
+// getStateList() {
+//   this.apiservice.getJsonObject('assets/data/states.json').subscribe(response => {
+//     let result: any = {};
+//     result = response;
+//     this.stateList = result;
+//   })
+// }
+// getCityList() {
+//   this.apiservice.getJsonObject('assets/data/usa-cities.json').subscribe((res) => {
+//     let result: any = {};
+//     result = res;
+//     this.cityList = result;
+//   })
+// }
 }
