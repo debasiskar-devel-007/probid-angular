36f964606280b751a2a7cb4a054ae5965d1318cb
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-12-11 06:31:04 +0530

    koushik

--- src/app/component/frontend/advance-inventory-search/advance-inventory-search.component.ts
+++ src/app/component/frontend/advance-inventory-search/advance-inventory-search.component.ts
@@ -8,6 +8,8 @@ import { MetaService } from '@ngx-meta/core';
 })
 export class AdvanceInventorySearchComponent implements OnInit {
 
+ 
+
   constructor(private readonly meta: MetaService) {
 
     this.meta.setTitle('ProBid Auto - Inventory');
