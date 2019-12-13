8c3e279bf09a3bae3f70696fcb71539a72ed6fb0
influxiq1 <himadribar.influxiq@gmail.com>
2019-12-13 12:39:36 +0530

    api-key

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -17,7 +17,7 @@ export class ApiService {
   public uploadurl =  environment["uploadurl"];
   public base64encode =  environment["base64encode"];
   public uploadsslurl: any= environment["download_url"];
-  public inventory_url: string = environment["inventory_url"];
+  public inventory_url: string = environment["inventory_url"] + environment["inventory_url_api"];
   public share_link : string = environment["share_link"];
 
   public fileimgsslurl: any;
